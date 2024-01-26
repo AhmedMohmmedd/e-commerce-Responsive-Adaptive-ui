c34ef40b85487a8b2cca7c03c03296879168bc7c
AhmedMohmmedd <https://github.com/AhmedMohmmedd>
2024-01-26 18:14:34 +0200

    add-assets

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,100 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppleAirPodspro
+  /// assets/images/appleAirPodspro.svg
+  static const String imagesAppleAirPodspro = "assets/images/appleAirPodspro.svg";
+
+  /// Assets for imagesBeatsSolo3Wireless
+  /// assets/images/beatsSolo3Wireless.svg
+  static const String imagesBeatsSolo3Wireless = "assets/images/beatsSolo3Wireless.svg";
+
+  /// Assets for imagesBeatsStudio3Bluetooth
+  /// assets/images/beatsStudio3Bluetooth.svg
+  static const String imagesBeatsStudio3Bluetooth = "assets/images/beatsStudio3Bluetooth.svg";
+
+  /// Assets for imagesBeatsnewstudioblueheadset
+  /// assets/images/beatsnewstudioblueheadset.svg
+  static const String imagesBeatsnewstudioblueheadset = "assets/images/beatsnewstudioblueheadset.svg";
+
+  /// Assets for imagesBeatsprowirelessheadset
+  /// assets/images/beatsprowirelessheadset.svg
+  static const String imagesBeatsprowirelessheadset = "assets/images/beatsprowirelessheadset.svg";
+
+  /// Assets for imagesCart
+  /// assets/images/cart.svg
+  static const String imagesCart = "assets/images/cart.svg";
+
+  /// Assets for imagesConectUs
+  /// assets/images/conectUs.svg
+  static const String imagesConectUs = "assets/images/conectUs.svg";
+
+  /// Assets for imagesEllipse15
+  /// assets/images/Ellipse15.svg
+  static const String imagesEllipse15 = "assets/images/Ellipse15.svg";
+
+  /// Assets for imagesEllipse16
+  /// assets/images/Ellipse16.svg
+  static const String imagesEllipse16 = "assets/images/Ellipse16.svg";
+
+  /// Assets for imagesEllipse17
+  /// assets/images/Ellipse17.svg
+  static const String imagesEllipse17 = "assets/images/Ellipse17.svg";
+
+  /// Assets for imagesEllipse18
+  /// assets/images/Ellipse18.svg
+  static const String imagesEllipse18 = "assets/images/Ellipse18.svg";
+
+  /// Assets for imagesExplore
+  /// assets/images/explore.svg
+  static const String imagesExplore = "assets/images/explore.svg";
+
+  /// Assets for imagesFromtopbrands
+  /// assets/images/fromtopbrands.svg
+  static const String imagesFromtopbrands = "assets/images/fromtopbrands.svg";
+
+  /// Assets for imagesGroup 23
+  /// assets/images/Group 23.svg
+  static const String imagesGroup23 = "assets/images/Group 23.svg";
+
+  /// Assets for imagesGroup 50
+  /// assets/images/Group 50.svg
+  static const String imagesGroup50 = "assets/images/Group 50.svg";
+
+  /// Assets for imagesHome
+  /// assets/images/home.svg
+  static const String imagesHome = "assets/images/home.svg";
+
+  /// Assets for imagesLenovoWiredHeadphone
+  /// assets/images/lenovoWiredHeadphone.svg
+  static const String imagesLenovoWiredHeadphone = "assets/images/lenovoWiredHeadphone.svg";
+
+  /// Assets for imagesOriginalBeatsSoloPro
+  /// assets/images/originalBeatsSoloPro.svg
+  static const String imagesOriginalBeatsSoloPro = "assets/images/originalBeatsSoloPro.svg";
+
+  /// Assets for imagesProfile
+  /// assets/images/profile.svg
+  static const String imagesProfile = "assets/images/profile.svg";
+
+  /// Assets for imagesSaved
+  /// assets/images/saved.svg
+  static const String imagesSaved = "assets/images/saved.svg";
+
+  /// Assets for imagesSeliing
+  /// assets/images/seliing.svg
+  static const String imagesSeliing = "assets/images/seliing.svg";
+
+  /// Assets for imagesSettings
+  /// assets/images/settings.svg
+  static const String imagesSettings = "assets/images/settings.svg";
+
+  /// Assets for imagesSoundix
+  /// assets/images/soundix.svg
+  static const String imagesSoundix = "assets/images/soundix.svg";
+
+  /// Assets for imagesSoundix2
+  /// assets/images/soundix2.svg
+  static const String imagesSoundix2 = "assets/images/soundix2.svg";
+}
+
---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static TextStyle styleMedium12Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF5C5C5C),
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium18Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF5B5959),
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleSemiBold14OpenSans(context) {
+    return TextStyle(
+       color:  Colors.black,
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Open Sans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleMedium14Roboto(context) {
+    return TextStyle(
+       color: const Color(0xFF00E0C6),
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Roboto',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium12Roboto(context) {
+    return TextStyle(
+       color: const Color(0xFF068181),
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontFamily: 'Roboto',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium16Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF009393),
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleSemiBold24Roboto(context) {
+    return TextStyle(
+       color: const Color(0xFF016170),
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontFamily: 'Roboto',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleMedium14Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF5C5C5C),
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleSemiBold14Roboto(context) {
+    return TextStyle(
+       color: const Color(0xFF5C5C5C),
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Roboto',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemiBold14Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF5C5C5C),
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleBold25PublicSans(context) {
+    return TextStyle(
+       color: const Color(0xFF016170),
+      fontSize: getResponsiveFontSize(context, fontSize: 25),
+      fontFamily: 'Public Sans',
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleSemiBold12Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF016170),
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleMedium26Poppins(context) {
+    return TextStyle(
+       color:  Colors.black,
+      fontSize: getResponsiveFontSize(context, fontSize: 26),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleRegular16Poppins(context) {
+    return TextStyle(
+       color:  Colors.black,
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleMedium20Poppins(context) {
+    return TextStyle(
+       color:  Colors.black,
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleRegular14Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF5C5C5C),
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular12_5Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF5C5C5C),
+      fontSize: getResponsiveFontSize(context, fontSize: 12.5),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular16OpenSans(context) {
+    return TextStyle(
+       color:  Colors.black,
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Open Sans',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleMedium15Poppins(context) {
+    return TextStyle(
+       color: const Color(0xFF016170),
+      fontSize: getResponsiveFontSize(context, fontSize: 15),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleSemiBold26Poppins(context) {
+    return TextStyle(
+       color: const Color(0xE5016170),
+      fontSize: getResponsiveFontSize(context, fontSize: 26),
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+}
+
+// sacleFactor
+// responsive font size
+// (min , max) fontsize
+double getResponsiveFontSize(context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * .8;
+  double upperLimit = fontSize * 1.2;
+
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(context) {
+  // var dispatcher = PlatformDispatcher.instance;
+  // var physicalWidth = dispatcher.views.first.physicalSize.width;
+  // var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
+  // double width = physicalWidth / devicePixelRatio;
+
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < 800) {
+    return width / 600;
+  } else if (width < 1050) {
+    return width / 900;
+  } else {
+    return width / 1300;
+  }
+}
